583f4fa3faf77a66ea62ee1546b7bbed057ab357
ROBIÑHO <asphalt9fr@gmail.com>
2023-10-01 22:16:44 +0200

    add : save page

--- src/routes/save/+page.svelte
+++ src/routes/save/+page.svelte
@@ -1,31 +1,128 @@
 <script lang="ts">
-let alreadySaved:boolean = false;
+let imageloaded= false;
+let positionloaded= false;
 
-    function showPosition() {
-        if (navigator.geolocation) {
-            alert(navigator.geolocation.getCurrentPosition(showPosition));
-        } else {
-            alert("Geolocation is not supported by this browser.");
-        }
+import { Checkbox } from '@svelteuidev/core';
+import Success from '$lib/Success.svelte';
+
+function showPosition() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log(position.coords.latitude+ ";" + position.coords.longitude);
+            localStorage.setItem('position', position.coords.latitude+ ";" + position.coords.longitude);
+            alert('Position saved!');
+            positionloaded = true;
+        });
+    } else {
+        alert("Geolocation is not supported by this browser.");
     }
+}
 
-    function submitFile(event){
+    function submitFile(event) {
         event.preventDefault();
-        const file = event.target.files[0];
-        const reader = new FileReader();
-        reader.onloadend = function() {
-            const base64String = reader.result;                
-            localStorage.setItem('image', base64String.toString());
+        const fileInput = document.getElementById('fileInput');
+        const file = fileInput.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = function () {
+                const base64String = reader.result;
+                localStorage.setItem('image', base64String.toString()); 
+                alert('Image saved!');
+
+            };
+            reader.readAsDataURL(file);
+            imageloaded = true;
+        } else {
+            alert('No file selected.');
         }
-        reader.readAsDataURL(file);
-        alert('Image saved!');
-    }
-    
+    }    
 </script>
 
-<input on:submit={submitFile} type="file" accept="image/*" />
+<div class="container">
+    <div class="needed">
+        hint : we need two things to remember where you parked your car, just fill above to check them
+        <li>
+            <Checkbox checked={imageloaded}/>
+            <div class="txt">your current location</div>
+        </li>
+        <li>
+            <Checkbox checked={positionloaded}/>
+            <div class="txt">a picture of your car parked</div>
+        </li>
+    </div>
 
-<div id="result">
-    <!--Position information will be inserted here-->
+    <div class="inputs">
+        <input id="fileInput" on:change={submitFile} type="file" accept="image/*" />
+        <button id="pos" type="button" on:click={showPosition}>Set Position</button>
+    </div>
 </div>
-<button type="button" on:click={showPosition}>Show Position</button>
+
+{#if imageloaded && positionloaded}
+<Success />
+{/if}
+
+
+<style>
+    .needed {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1rem;
+        background-color: rgb(106, 207, 143);
+        border-radius: 20px;
+        padding: 20px;
+        color: white;
+        position: absolute;
+        bottom: 5px;
+        width: 80%;
+    }
+
+    li{
+        list-style-type: none;
+        display: flex;  
+        flex-direction: row;
+        justify-content: center;
+        margin: 10px;
+    }
+
+    .txt {
+        margin-left: 10px;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1rem;
+        margin-bottom: 2rem;
+        border-radius: 30px;
+        color: white;
+        background-color: blue;
+    }
+
+    .inputs {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1rem;
+        margin-bottom: 2rem;
+        border-radius: 20px;
+        padding: 20px;
+        color: white;
+        background-color: blue;
+        width: 80%;
+    }
+
+    input {
+        margin-left: 20px;
+        font-size: 1rem;
+    }
+
+    #pos {
+        margin-top: 20px;
+    }
+</style>

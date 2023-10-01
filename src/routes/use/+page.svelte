c4951ffde472c5cac27cac27dbce323550191ca3
ROBIÑHO <asphalt9fr@gmail.com>
2023-10-01 22:16:56 +0200

    use page

--- src/routes/use/+page.svelte
+++ src/routes/use/+page.svelte
@@ -6,36 +6,53 @@
     import { goto } from '$app/navigation';
     //width={window.innerWidth} height={0.6*window.innerHeight}
     
-    let alreadySaved:boolean = false;
+    let loading = true; 
+    let alreadySaved = false;
+    let savedImage;
 
     onMount(() => {
         //check if a image is stored in local storage, plus its location
-        if (localStorage.getItem('image') !== null && localStorage.getItem('location') !== null) {
+        if (localStorage.getItem('image') !== null && localStorage.getItem('position') !== null) {
+            savedImage = localStorage.getItem('image');
             alreadySaved = true;
         }
+        loading = false;
     })
 
     function handleClick() {
         alert('Button clicked!');
     }
-
-
 </script>
+
+{#if loading}    
+<div class="loader-container">
+    <div class="loader"></div>
+</div>
+
+{:else}
+
 {#if alreadySaved}
-<Image radius='md' src="https://picsum.photos/200/300" alt="Random image" />
+<Image radius='md' src={savedImage} alt="Your parking spot" />
 {:else}
 <Title order={4}>looks like you have never saved your car last position</Title>
 {/if}
+
 <div class='buttons'>
-<Button on:click={()=>goto("/save")} variant='gradient' gradient={{from: 'teal', to: 'blue', deg: 60}}>
-    <i class="fa-solid fa-location-pin"></i><span class="narrow-space"></span> Save the location
-</Button>
 {#if alreadySaved}
+<Button on:click={()=>goto("/save")} variant='gradient' gradient={{from: 'teal', to: 'blue', deg: 60}}>
+    <i class="fa-solid fa-location-pin"></i><span class="narrow-space"></span> Change the location
+</Button>
 <Button on:click={handleClick} variant='gradient' gradient={{from: 'orange', to: 'red', deg: 45}}>
 	<i class="fa-solid fa-location-crosshairs"></i><span class="narrow-space"></span> Waypoint
 </Button>
+{:else}
+<Button on:click={()=>goto("/save")} variant='gradient' gradient={{from: 'teal', to: 'blue', deg: 60}}>
+    <i class="fa-solid fa-location-pin"></i><span class="narrow-space"></span> Save the location
+</Button>
 {/if}
 </div>
+
+{/if}
 
 <style>
     .buttons {
@@ -49,5 +66,26 @@
     .narrow-space {
             width: 10px;
             display: inline-block;
+    }
+
+    .loader-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .loader {
+        border: 4px solid rgba(0, 0, 0, 0.2);
+        border-top: 4px solid #007bff; /* Loader color */
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        animation: spin 2s linear infinite; /* Animation duration */
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
     }
 </style>

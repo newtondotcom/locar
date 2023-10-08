7a656ce9bd22cca48383a1b3209a5a2462e1bca8
newtondotcom <asphalt9fr@gmail.com>
2023-10-08 13:30:58 +0200

    add : fullscreen + code indentation

--- src/routes/use/+page.svelte
+++ src/routes/use/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
     import { goto } from '$app/navigation';
-    //width={window.innerWidth} height={0.6*window.innerHeight}
+    import NProgress from "nprogress";
     
     let loading = true; 
     let alreadySaved = false;
@@ -22,6 +22,7 @@
     })
 
     function handleWaypoint() {
+        NProgress.start();
         const isMobile = /Android|iOS|iPhone|iPad|iPod|Windows Phone/i.test(navigator.userAgent);
 
         if (isMobile) {
@@ -39,33 +40,48 @@
         } else {
             alert('Navigation is only supported on mobile devices.');
         }
+        NProgress.done();
     }
+
+    function openFullScreen() {
+        let w = window.open('about:blank');
+        let image = new Image();
+        image.src = savedImage;
+        setTimeout(function(){
+        w.document.write(image.outerHTML);
+}, 0);
+    }
+
 </script>
 
+<div class="flex flex-col justify-center items-center h-screen">
 {#if loading}    
-<span class="loading loading-spinner loading-lg"></span>
+    <span class="loading loading-spinner loading-lg"></span>
 {:else}
 
-{#if alreadySaved}
-{#if savedPosition}    
-<button class="btn btn-outline btn-success  w-2/3 mx-auto">Waypoint to last location</button>
-{/if}
-{#if savedImage}
-    <div class="container"><img src={savedImage} alt="Your parking spot" /></div>
-{/if}
-{:else}
-    <h4>looks like you have never saved your car last position</h4>
-{/if}
+    {#if alreadySaved}
+        {#if savedPosition}    
+            <button class="btn btn-outline btn-success  w-2/3 mx-auto" on:click={handleWaypoint}>Waypoint to last location</button>
+        {/if}
+        {#if savedImage}
+            <div class="container">
+                <img src={savedImage} alt="Your parking spot" />
+                <button on:click={openFullScreen} class="btn btn-active btn-secondary">See it in full screen</button>
+            </div>
+        {/if}
+    {:else}
+        <h4>looks like you have never saved your car last position</h4>
+    {/if}
 
-<div class='buttons'>
-{#if alreadySaved}
-<button on:click={()=>goto("/save")} class="btn btn-outline w-2/3 mx-auto btn-info">Change the previous location</button>
-{:else}
-<button on:click={()=>goto("/save")} class="btn btn-outline btn-warning w-2/3 mx-auto">Save your location</button>
+    <div class='buttons'>
+        {#if alreadySaved}
+            <button on:click={()=>goto("/save")} class="btn btn-outline w-2/3 mx-auto btn-info">Change the previous location</button>
+        {:else}
+            <button on:click={()=>goto("/save")} class="btn btn-outline btn-warning w-2/3 mx-auto">Save your location</button>
+        {/if}
+    </div>
 {/if}
 </div>
-
-{/if}
 
 <style lang="postcss">
     .buttons {

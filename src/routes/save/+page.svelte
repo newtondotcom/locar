163d38ef38b52a331401f4436a84476615c6fa32
newtondotcom <asphalt9fr@gmail.com>
2023-10-09 22:50:30 +0200

    fix : save page

--- src/routes/save/+page.svelte
+++ src/routes/save/+page.svelte
@@ -29,7 +29,7 @@ import constants from "$lib/constants";
             reader.onloadend = function () {
                 const base64String = reader.result;
                 localStorage.setItem('image', base64String.toString()); 
-                addToast({ message: "The image has been saved ! ", type: "success", dismissible: true, timeout: constants.delayNotification });
+                addToast({ message: "The image has been saved ! ", dismissible: true, timeout: constants.delayNotification });
             };
             reader.readAsDataURL(file);
             imageloaded = true;
@@ -39,20 +39,22 @@ import constants from "$lib/constants";
     }    
 </script>
 
-<div class="pre">you can choose to input your car location and a picture of it (to remember your park number for ex), or just one of them</div>
+<div class="flex flex-col justify-center mt-2 mb-8 rounded-lg p-5 text-black sm:text-5xl">
+    you can choose to input your car location and a picture of it (to remember your park number for ex), or just one of them
+</div>
     <div class="flex flex-col justify-center items-center h-screen w-full border-opacity-50 text-black-800 text-xl font-semibold">
-        <div class="grid h-50 card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
+        <div class="grid xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
             <i class="fa-solid fa-camera"></i> Picture of your car 
-            <input id="fileInput" class="file-input file-input-bordered w-2/3 max-w-xs m-8" on:change={submitFile} type="file" accept="image/*"/>
+            <input id="fileInput" class="file-input file-input-bordered w-2/3 max-w-xs m-8 sm:text-5xl sm:w-full" on:change={submitFile} type="file" accept="image/*"/>
         </div>
-        <div class="divider">AND / OR</div>
-        <div class="grid h-30 card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
+        <div class="divider sm:text-4xl">AND / OR</div>
+        <div class="grid xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
             <i class="fa-solid fa-location-crosshairs"></i> Location
-                <button class="btn btn-active btn-secondary" on:click={submitPosition}>                
+                <button class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 sm:text-4xl sm:h-20 items-center justify-center h-screen" on:click={submitPosition}>                
                     {#if positionloaded}
-                    <svg viewBox="0 0 24 24" class="h-4 w-4" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                        <svg viewBox="0 0 24 24" class="h-4 w-4" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 14L9 19L20 8M6 8.88889L9.07692 12L16 5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                     {:else}    
-                        Set Position
+                        <div class="sm:text-4xl">Set Position</div>
                     {/if}
                 </button>
         </div>
@@ -86,17 +88,5 @@ import constants from "$lib/constants";
 
     i {
         margin : 10px;
-    }
-
-    .pre {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin-top: 0.5rem;
-        margin-bottom: 2rem;
-        border-radius: 20px;
-        padding: 20px;
-        color: black;
-        width: 80%;
     }
 </style>

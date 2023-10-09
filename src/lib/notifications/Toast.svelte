aee54c5a0dc9df3fe12709f0d6c9d9c2bdc95f69
newtondotcom <asphalt9fr@gmail.com>
2023-10-09 22:49:46 +0200

    fix : toats ui update

--- src/lib/notifications/Toast.svelte
+++ src/lib/notifications/Toast.svelte
@@ -5,17 +5,44 @@
     const dispatch = createEventDispatcher();
     export let dismissible = true;
   </script>
-  
-  <div class="toast toast-center">
-    <div class="alert alert-info">
-      <span><slot /></span>
+
+  <div role="alert" class="rounded-xl border border-gray-100 bg-white p-4">
+    <div class="flex items-start gap-4">
       {#if dismissible}
       <button class="close" on:click={() => dispatch("dismiss")}>
         <CloseIcon width="0.8em" />
       </button>
       {/if}
+  
+      <div class="flex-1">
+        <strong class="block font-medium text-gray-900"> Success </strong>
+  
+        <p class="mt-1 text-sm text-gray-700">
+          <slot />
+        </p>
+      </div>
+  
+      <button class="text-gray-500 transition hover:text-gray-600">
+        <span class="sr-only">Dismiss popup</span>
+  
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="h-6 w-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
     </div>
   </div>
+  
   
   <style lang="postcss">
     button {

55dead30c00eef3d30db23b62bf069bd7189352d
ROBIÑHO <asphalt9fr@gmail.com>
2023-10-11 09:41:18 +0200

    fix : save page

--- src/routes/save/+page.svelte
+++ src/routes/save/+page.svelte
@@ -43,12 +43,12 @@ import constants from "$lib/constants";
     you can choose to input your car location and a picture of it (to remember your park number for ex), or just one of them
 </div>
     <div class="flex flex-col justify-center items-center h-screen w-full border-opacity-50 text-black-800 text-xl font-semibold">
-        <div class="grid xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
+        <div class="grid bg-slate-400 xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
             <i class="fa-solid fa-camera"></i> Picture of your car 
             <input id="fileInput" class="file-input file-input-bordered w-2/3 max-w-xs m-8 sm:text-5xl sm:w-full" on:change={submitFile} type="file" accept="image/*"/>
         </div>
         <div class="divider sm:text-4xl">AND / OR</div>
-        <div class="grid xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
+        <div class="grid bg-slate-400 xl:h-50 sm:h-200 sm:text-5xl card bg-base-300 rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
             <i class="fa-solid fa-location-crosshairs"></i> Location
                 <button class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 sm:text-4xl sm:h-20 items-center justify-center h-screen" on:click={submitPosition}>                
                     {#if positionloaded}
@@ -72,10 +72,10 @@ import constants from "$lib/constants";
         border: 1px solid white;
         border-radius: 20px;
         padding: 20px;
-        color : black;
         width: auto;
         height: 60px;
         background-color: white;
+        color:black;
     }
 
     i{

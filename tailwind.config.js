c640ea29eedf537182e6ec4e2d4146a2bffcd25e
newtondotcom <asphalt9fr@gmail.com>
2024-01-14 13:42:39 +0100

    screen marker + new logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {  
+        'mlg': {'max': '1023px'},
+        'mmd': {'max': '767px'},
+        'msm': {'max': '639px'},
+      }
+    },
   },
-  plugins: [require("daisyui")],
-}
-
+  plugins: [],
+};

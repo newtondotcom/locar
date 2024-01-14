0b82727b323e159e95f3bc7eff6e43f5adac7dcc
newtondotcom <asphalt9fr@gmail.com>
2024-01-14 13:41:56 +0100

    ui and ux improvement

--- src/lib/routing/Save.svelte
+++ src/lib/routing/Save.svelte
@@ -42,18 +42,18 @@
     </script>
     
         <div class="flex flex-col justify-center items-center h-screen w-full border-opacity-50 text-black text-xl font-semibold">
-            <div class="grid bg-slate-400 xl:h-50 sm:h-200 sm:text-4xl card rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
-                <i class="fa-solid fa-camera"></i> {translate("picture")}
-                <input id="fileInput" class="file-input file-input-bordered w-2/3 max-w-xs m-8 sm:text-5xl sm:w-full" on:change={submitFile} type="file" accept="image/*"/>
+            <div class="grid bg-gray-300 rounded-xl h-200 text-4xl card rounded-box place-items-center w-4/5 mx-4 my-7 pt-8">
+                {translate("picture")}
+                <input id="fileInput" class="file-input file-input-bordered max-w-xs  m-8 text-4xl file:w-full file:border file:rounded-full file:border-blue-600 file:bg-blue-600 file:px-12 file:py-3 file:font-medium file:text-white hover:file:bg-transparent hover:file:text-blue-600 focus:file:outline-none focus:ring active:file:text-blue-500 file:text-4xl file:items-center file:justify-center" on:change={submitFile} type="file" accept="image/*"/>
             </div>
-            <div class="divider sm:text-4xl">{translate("andor")}</div>
-            <div class="grid bg-slate-400 xl:h-50 sm:h-200 sm:text-4xl card rounded-box place-items-center m-4 xl:w-2/3 sm:w-4/5">
-                <i class="fa-solid fa-location-crosshairs"></i> {translate("position")}
-                    <button class="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 sm:text-4xl sm:h-20 items-center justify-center h-screen" on:click={submitPosition}>                
+            <div class="divider text-4xl">{translate("andor")}</div>
+            <div class="grid bg-gray-300 rounded-xl h-200 text-4xl card rounded-box place-items-center w-4/5 mx-4 my-7 pt-8">
+                {translate("position")}
+                    <button class="inline-block rounded-full border border-blue-600 bg-blue-600 px-12 py-3 font-medium text-white hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-white text-4xl items-center justify-center m-8" on:click={submitPosition}>                
                         {#if positionloaded}
-                            <svg viewBox="0 0 24 24" class="h-4 w-4" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                            <svg viewBox="0 0 24 24" class="h-4 w-4 text-white " fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
                         {:else}    
-                            <div class="sm:text-4xl">{translate("setpos")}</div>
+                            <div class="text-4xl">{translate("setpos")}</div>
                         {/if}
                     </button>
             </div>
@@ -62,30 +62,8 @@
     <Success bind:imageloaded bind:positionloaded goToRecent={goToHome} />
     
     
-    <style lang="postcss">
-        button {
-            display: flex;  
-            flex-direction: row;
-            justify-content: center;
-            margin: 10px;
-            border: 1px solid white;
-            border-radius: 20px;
-            padding: 20px;
-            width: auto;
-            height: 60px;
-            background-color: white;
-            color:black;
-        }
-    
-        i{
-            transform: translateY(2px);
-        }
-    
+    <style lang="postcss">    
         input {
             font-size: 1rem;
-        }
-    
-        i {
-            margin : 10px;
         }
     </style>
